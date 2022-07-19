787bf5f7e4cb73274c202aaf9db0cbbd769f6c32
Marc <marcbaque1311@gmail.com>
2022-07-20 00:31:59 +0200

    [ ADD ] some test api endpoints

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,5 +1,29 @@
+use rocket::serde::json::{json, Value};
+
 mod models;
 
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+const API_BASE: &str = "/api";
+
+#[get("/")]
+fn hello_world() -> &'static str {
+    "Hello World"
+}
+
+/// API route not found catcher that returns a plain JSON response.
+#[catch(404)]
+fn not_found() -> Value {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount(API_BASE, routes![hello_world])
+        .register(API_BASE, catchers!(not_found))
 }
