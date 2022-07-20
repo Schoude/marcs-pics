a36a27b471b45c79957e84529a9cf15264dc50c4
Marc <marcbaque1311@gmail.com>
2022-07-21 00:15:35 +0200

    [ ADD ] use bcrypt to hash the new users pw

--- api/src/handlers/user/mod.rs
+++ api/src/handlers/user/mod.rs
@@ -1,5 +1,7 @@
-use crate::{db::mongodb::MongoORM, models::user::User};
+extern crate bcrypt;
 
+use crate::{db::mongodb::MongoORM, models::user::User};
+use bcrypt::hash;
 use mongodb::results::InsertOneResult;
 use rocket::{http::Status, serde::json::Json, State};
 
@@ -12,12 +14,16 @@ pub fn add_user(
     // TODO: check if user with the email/username already exists
     // Return Status::UnprocessableEntity
 
+    let hashed_pw = match hash(user.password.to_owned(), 4) {
+        Ok(val) => val,
+        Err(_) => panic!("Error hashing to password."),
+    };
+
     let new_user = User {
         id: None,
         nickname: user.nickname.to_owned(),
         email: user.email.to_owned(),
-        // TODO: bcrypt the password!
-        password: user.password.to_owned(),
+        password: hashed_pw,
         role: user.0.role,
     };
 
