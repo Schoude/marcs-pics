3b60ba08d871cb0a362547369e60efaea5976ca0
Marc <marcbaque1311@gmail.com>
2022-07-20 22:32:48 +0200

    [ ADD ] json response test endpont

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,7 +2,10 @@ mod handlers;
 mod models;
 
 use handlers::user::add_user;
-use rocket::serde::json::{json, Value};
+use rocket::{
+    http::Status,
+    serde::json::{json, Json, Value},
+};
 
 #[macro_use]
 extern crate rocket;
@@ -12,6 +15,11 @@ const API_BASE: &str = "/api";
 #[get("/")]
 fn hello_world() -> &'static str {
     "Hello World"
+}
+
+#[get("/hello")]
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello from Rust with MongoDB!")))
 }
 
 /// API route not found catcher that returns a plain JSON response.
@@ -26,6 +34,6 @@ fn not_found() -> Value {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount(API_BASE, routes![hello_world, add_user])
+        .mount(API_BASE, routes![hello_world, hello, add_user])
         .register(API_BASE, catchers!(not_found))
 }
