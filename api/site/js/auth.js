7799e1309264645312eea5a6ac72077a0a6fa2ae
Marc <marcbaque1311@gmail.com>
2022-07-30 16:46:34 +0200

    [ UPDATE ] mode login call to auth module

--- api/site/js/auth.js
+++ api/site/js/auth.js
@@ -1,3 +1,17 @@
+async function loginUser(credentials) {
+  const res = await fetch('/api/login', {
+    method: 'POST',
+    body: JSON.stringify(credentials),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (res.status === 202) {
+    window.location.pathname = ('/');
+  } else {
+    console.log('show error');
+  }
+}
+
 async function logoutUser() {
   const res = await fetch('/api/logout', { method: 'POST' });
   if (res.status === 202) {
@@ -21,6 +35,7 @@ async function checkAuthLoginPage() {
 }
 
 export {
+  loginUser,
   logoutUser,
   checkAuth,
   checkAuthLoginPage,
